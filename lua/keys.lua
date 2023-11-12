5f6cc8dc027cf3eb7e7a1373039d9c1a360c00a8
Niza Toshpulatov <niza@hey.com>
2023-11-12 17:07:25 +0100

    Remove auto-save and backup files, add tab renaming

--- lua/keys.lua
+++ lua/keys.lua
@@ -36,4 +36,19 @@ return {
 		mods = "SHIFT",
 		action = wezterm.action.SendKey({ key = "Backspace" }),
 	},
+  {
+    key = "r",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.PromptInputLine {
+      description = "Enter new name for the current tab",
+      action = wezterm.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
 }
